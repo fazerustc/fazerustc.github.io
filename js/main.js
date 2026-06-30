d19516ab900723336ed7066f4aada444022081c1
FaZe rustc <fazerustc@gmail.com>
2026-06-30 01:41:13 -0700

    Add data

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", init, false); // init once loaded
 
 var searchInput = undefined;
-var searchType = undefined;
+var searchKind = undefined;
 const BGS = ["bg-slate-100", "bg-gray-200"];
 
 function init() {
     getJsonData();
 
     searchInput = document.getElementById('searchInput');
-    searchType = document.getElementById("searchType");
+    searchKind = document.getElementById("searchKind");
 
     searchInput.addEventListener('input', filterTable);
-    searchType.addEventListener('change', (event) => { filterTable() });
+    searchKind.addEventListener('change', (event) => { filterTable() });
 }
 
 //this function is in the event listener and will execute on page load
@@ -28,15 +28,12 @@ function getJsonData(){
             appendJson(data);// pass the json object to the appendJson function
         }
     }
-    //set the request destination and type
-    xmlhttp.open("POST", jsonUrl, true);
-    //set required headers for the request
+    xmlhttp.open("GET", jsonUrl, true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // send the request
     xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section
 }
 
-//this function appends the json data to the table 'gable'
+//this function appends the json data to the table 'dataTable'
 function appendJson(data){
     var table = document.getElementById('dataTable');
     var i = 0;
@@ -45,9 +42,9 @@ function appendJson(data){
         tr.classList.add(BGS[i % BGS.length]);
         i += 1;
         tr.setAttribute("filter-name", object.name);
-        tr.setAttribute("filter-type", object.type);
+        tr.setAttribute("filter-kind", object.kind);
         tr.innerHTML = '<th scope="row" class="px-6 py-4 font-medium text-heading whitespace-nowrap><a href="' + object.url + '">' + object.display + '</a></th>' +
-        '<td scope="row" class="px-6 py-4">' + object.type + '</td>';
+        '<td scope="row" class="px-6 py-4">' + object.kind+ '</td>';
         table.tBodies[0].appendChild(tr);
     });
 }
@@ -56,7 +53,7 @@ function filterTable() {
     console.log("Filtering");
     const tableRows = document.querySelectorAll('#dataTable tbody tr');
     const filterValue = searchInput.value.toLowerCase();
-    const typeValue = searchType.value.toLowerCase();
+    const kindValue = searchKind.value.toLowerCase();
     var regex = undefined;
     try {
         if (filterValue) {
@@ -71,7 +68,7 @@ function filterTable() {
     tableRows.forEach(row => {
         // Get all text content within the row cells
         const rowName = row.getAttribute("filter-name").toLowerCase();
-        const rowType = row.getAttribute("filter-type").toLowerCase();
+        const rowKind = row.getAttribute("filter-kind").toLowerCase();
         for (var j = 0; j < BGS.length; j++) {
             row.classList.remove(BGS[j]);
         }
@@ -79,11 +76,9 @@ function filterTable() {
         i += 1;
         
         // If the row contains the search term, display it; otherwise, hide it
-        if ((!regex || regex.test(rowName)) && (typeValue == "any" || typeValue == rowType)) {
-            console.log(rowName + " matches " + filterValue);
+        if ((!regex || regex.test(rowName)) && (kindValue == "any" || kindValue == rowKind)) {
             row.style.display = '';
         } else {
-            console.log(rowName + " does not match " + filterValue + " " + typeValue);
             row.style.display = 'none';
         }
     });
---
+++ scrape.py
@@ -0,0 +1,156 @@
+import argparse
+from dataclasses import dataclass
+import json
+import re
+
+
+from bs4 import BeautifulSoup as BS
+import cloudscraper
+
+
+POKEMON = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+ITEMS = "https://bulbapedia.bulbagarden.net/wiki/List_of_items_by_name"
+MOVES = "https://bulbapedia.bulbagarden.net/wiki/List_of_moves"
+ABILITIES = "https://bulbapedia.bulbagarden.net/wiki/Ability"
+CHARACTERS = "https://bulbapedia.bulbagarden.net/wiki/List_of_game_characters"
+ROOT = "https://bulbapedia.bulbagarden.net"
+
+
+@dataclass
+class Data:
+    name: str
+    url: str
+    kind: str
+    display: str
+
+    def to_json(self) -> dict[str, str]:
+        return {
+            "name": self.name,
+            "url": self.url,
+            "kind": self.kind,
+            "display": self.display,
+        }
+
+
+def scrape(url: str, selector, kind: str, needle, ignore: set[str]) -> list[Data]:
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url)
+    soup = BS(response.text, "html.parser")
+    tags = (selector)(soup)
+    result = {}
+    for tag in tags:
+        href = tag.get("href")
+        if not href:
+            continue
+        matches = re.match(needle, href)
+        if matches:
+            display = (
+                matches.group(1)
+                .replace("_", " ")
+                .replace("%C3%A9", "é")
+                .replace("%C3%89", "É")
+                .replace("%27", "'")
+                .replace("%26", "&")
+                .replace("%C5%AB", "ū")
+                .replace("%C5%8D", "ō")
+                .replace("%C3%B1", "ñ")
+                .replace("%3F", "?")
+                .replace("%E2%99%80", "♂")
+                .replace("%E2%99%82", "♀")
+                .strip()
+            )
+            name = (
+                display.replace(" ", "")
+                .replace("é", "e")
+                .replace("-", "")
+                .replace(".", "")
+                .replace(",", "")
+                .replace("'", "")
+                .replace("&", "")
+                .replace("ū", "u")
+                .replace("ō", "o")
+                .replace("ñ", "n")
+                .replace("?", "")
+                .replace("♂", "")
+                .replace("♀", "")
+                .lower()
+            )
+            if name in ignore:
+                continue
+            if name.startswith("file:"):
+                continue
+            if name.startswith("candy#"):
+                continue
+            result[name] = Data(
+                name=name, url=f"{ROOT}{href}", kind=kind, display=display
+            )
+    return list(result.values())
+
+
+def main(outfile: str):
+    data = []
+    data += scrape(
+        ABILITIES,
+        lambda soup: soup.find_all("a"),
+        "Ability",
+        re.compile("^/wiki/(.*)_\(Ability\)$"),
+        set(),
+    )
+    data += scrape(
+        CHARACTERS,
+        lambda soup: soup.select("tr td a"),
+        "Character",
+        re.compile("^/wiki/([^(]*)"),
+        {
+            "bulbapedia:projectcharacterdex",
+            "bulbapedia:projects",
+            "category:characters",
+            "file:projectcharacterdexlogo.png",
+            "listofanimatedseriescharacters",
+            "listofpokemonadventurescharacters",
+            "listofpokemonconquestcharacters",
+            "listofpokemonconquestcharacters",
+            "listofpokemonmysterydungeon",
+            "listofpokemonmysterydungeon:explorersoftime,darkness,andskycharacters",
+            "listofpokemonmysterydungeon:gatestoinfinitycharacters",
+            "listofpokemonmysterydungeon:redrescueteamandbluerescueteamcharacters",
+            "listofpokemonsupermysterydungeoncharacters",
+            "pokemongames",
+            "pokemonmysterydungeonseries",
+        },
+    )
+    data += scrape(
+        ITEMS,
+        lambda soup: [
+            tr.select_one("td:nth-of-type(2)").select_one("a")
+            for tr in soup.select("tr")
+            if tr.select_one("td:nth-of-type(2)")
+        ],
+        "Item",
+        re.compile("^/wiki/(.*)$"),
+        {"bulbapedia:projectitemdex"},
+    )
+    data += scrape(
+        MOVES,
+        lambda soup: soup.find_all("a"),
+        "Move",
+        re.compile("^/wiki/(.*)_\(move\)$"),
+        set(),
+    )
+    data += scrape(
+        POKEMON,
+        lambda soup: soup.find_all("a"),
+        "Pokémon",
+        re.compile("^/wiki/(.*)_\(Pok%C3%A9mon\)$"),
+        set(),
+    )
+    data.sort(key=lambda d: d.name)
+    with open(outfile, "w") as f:
+        json.dump([d.to_json() for d in data], f, sort_keys=True, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--outfile", default="data/data.json")
+    args = parser.parse_args()
+    main(args.outfile)
